f16d0751d581fcca5ea7e9292cbe481ce9eff6aa
Jassadar <jassadar@jassadar-EasyNote-TJ65.(none)>
2012-03-09 17:11:52 +0000

    funzione filter migliorata

--- filter_catalogo.js
+++ filter_catalogo.js
@@ -1,17 +1,23 @@
 
-function filter(value) {
+function filter(value, theCleckBox) {
 	var div_array = document.getElementsByClassName("pr_container");
 
 	for(i=0; i<div_array.length; i++){
 		var dd_array = div_array[i].getElementsByClassName("tipologia");
-		
-		if(dd_array[0].innerHTML == value)
+
+		if(!theCleckBox.checked)
 		{
-			
+			if(dd_array[0].innerHTML == value)
+			{
+				div_array[i].style.display='none';
+			}
 		}
 		else
 		{
-			alert("non ce");
+			if(dd_array[0].innerHTML == value)
+			{
+				div_array[i].style.display='';
+			}
 		}
 	}
 
