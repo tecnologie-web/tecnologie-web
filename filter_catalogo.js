dd3b600df18cd840d08481851294205b892ef54c
Jassadar <jassadar@jassadar-EasyNote-TJ65.(none)>
2012-03-09 16:31:57 +0000

    modificato file js

--- filter_catalogo.js
+++ filter_catalogo.js
@@ -1,4 +1,18 @@
 
-function filterById(id_var) {
-	document.getElementByID(id_var).value
+function filter(value) {
+	var div_array = document.getElementsByClassName("pr_container");
+
+	for(i=0; i<div_array.length; i++){
+		var dd_array = div_array[i].getElementsByClassName("tipologia");
+		
+		if(dd_array[0].innerHTML == value)
+		{
+			
+		}
+		else
+		{
+			alert("non ce");
+		}
+	}
+
 }
