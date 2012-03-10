9c5779961b2295dd53b39fb8050ed7280715da5c
Jassadar <jassadar@jassadar-EasyNote-TJ65.(none)>
2012-03-10 12:05:53 +0000

    ridotta lista di parametri filter()

--- filter_catalogo.js
+++ filter_catalogo.js
@@ -1,5 +1,24 @@
+//function filterByName(theInput) {
+//	var div_array = document.getElementsByClassName("pr_container");
+//
+//	for(i=0; i<div_array.length; i++){
+//		var dt_array = div_array[i].getElementsByTagName("dd");
+//		for(j=0; j<dd_array.length; j++){
+//			if(dd_array[j].innerHTML != theInput.value)
+//			{
+//				div_array[i].style.display='none';
+//			}
+//
+//			else
+//			{
+//				div_array[i].style.display='';
+//			}
+//		}
+//
+//	}
+//}
 
-function filter(value, theCleckBox) {
+function filter(theCleckBox) {
 	var div_array = document.getElementsByClassName("pr_container");
 
 	for(i=0; i<div_array.length; i++){
@@ -7,14 +26,14 @@ function filter(value, theCleckBox) {
 		for(j=0; j<dd_array.length; j++){
 			if(!theCleckBox.checked)
 			{
-				if(dd_array[j].innerHTML == value)
+				if(dd_array[j].innerHTML == theCleckBox.value)
 				{
 					div_array[i].style.display='none';
 				}
 			}
 			else
 			{
-				if(dd_array[j].innerHTML == value)
+				if(dd_array[j].innerHTML == theCleckBox.value)
 				{
 					div_array[i].style.display='';
 				}
